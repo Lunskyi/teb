ee98f8ddde90d3b8395c30e1cbb6da922a7af3dd
Lunskyi <92211469+Lunskyi@users.noreply.github.com>
2021-10-09 14:50:46 +0200

    teb

---
+++ 2_php/1_index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Strona Glówna</title>
+  </head>
+  <body>
+    <p>Strona Glówna</p>
+    
+  </body>
+</html>
