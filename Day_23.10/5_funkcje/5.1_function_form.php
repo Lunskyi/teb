23eff5a27be96fb8099fc8dbf8c3a94fb7e2c265
Lunskyi <lovckach@gmail.com>
2021-10-26 19:19:59 +0200

    полное описание 5_funkcje

--- Day_23.10/5_funkcje/5.1_function_form.php
+++ Day_23.10/5_funkcje/5.1_function_form.php
@@ -1,30 +1,30 @@
 <?php
-session_start();
+session_start(); //запуск сессии, всегда пишется в начале
  ?>
 <!DOCTYPE html>
 <html lang="pl" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="./css/style.css">
+     <link rel="stylesheet" href="./css/style.css"> <!--указываем с какого файла берем шаблоны стиля, или сам стиль... -->
     <title>Formularz</title>
   </head>
   <body>
     <?php
-      if (isset($_SESSION['error'])) {
-        echo "<div class=\"red\">".$_SESSION['error']."</div>";
-        unset($_SESSION['error']);
+      if (isset($_SESSION['error'])) {  /*задаем условие, если значение сессии существует, отлично от NULL, (в данном случае значение получаем
+        с адресса ./scripts/script.php)*/
+        echo "<div class=\"red\">".$_SESSION['error']."</div>"; // выводим в шапке страници значение нашей сессии, так же указываем шаблон стиля red
+        unset($_SESSION['error']);// удаляем переменную действующей сессии
       }
-      if (isset($_SESSION['ok'])) {
-        echo "<div class=\"red\">".$_SESSION['ok']."</div>";
-        unset($_SESSION['ok']);
-      }
-     ?>
-    <h3>Dane Uzytkownika</h3>
-    <form action="./scripts/script.php" method="post">
-      <input type="text" name="name" placeholder="Podaj imię">
-      <input type="text" name="surname" placeholder="Podaj nazwisko">
-      <input type="date" name="birthday"> Data urodzenia <br> <br>
-      <input type="submit" value="Zatwierdż">
+    ?>
+  <h3>Dane Uzytkownika</h3>
+    <form action="./scripts/script.php" method="post"> <!--создаем форму указав безопасный способ отправки данных пользователя method="post".
+      так же указываем что данные с формы будут отправленны по адресу action="./scripts/script.php" -->
+      <input type="text" name="name" placeholder="Podaj imię">  <!-- создаем текстовый элементформы с именем name (name="name") с шаблоно-исчезаюзим
+      значение placeholder="Podaj imię"       -->
+      <input type="text" name="surname" placeholder="Podaj nazwisko"><!-- создаем текстовый элементформы с именем surname (name="surname") с шаблоно-исчезаюзим
+      значение placeholder="Podaj nazwisko"       -->
+      <input type="date" name="birthday"> Data urodzenia <br> <br><!-- создаем  элемент формы календарь, с именем name (name="birthday")  -->
+      <input type="submit" value="Zatwierdż"><!-- создаем элемент формы кнопку, для отправки формы (данные что ввел пользователь) -->
     </form>
   </body>
 </html>
--- Day_23.10/5_funkcje/5.1_result.php
+++ Day_23.10/5_funkcje/5.1_result.php
@@ -1,11 +1,11 @@
 <?php
-session_start();
- var_dump($_SESSION);
+session_start(); //запускаем сессию, в данном случае указываем что этот файл тоже работает с основной сессией
+ //var_dump($_SESSION); //вывод значения сессии
 // echo "<pre>";
- echo print_r($_SESSION);
+ //print_r($_SESSION); // вывод значение сессии в вертикальной форме
 // echo "</pre>";
  ?>
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="pl" dir="ltr">
   <head>
     <meta charset="utf-8">
@@ -13,22 +13,27 @@ session_start();
   <title>Dane z formularza</title>
   <body>
     <?php
-//print_r($_SESSION['data']);
-// echo "<h3>Dane pobrane z formularza</h3>";
-// foreach ($_SESSION['data'] as $key => $value) {
-// //echo "<br>".$key . " - " . $value . "<br>";
-// echo "$key : $value, dlugość: " .strlen($value). "<br>";
-// }
-// echo "<hr><h3>Poprawione dane: </h3>";
-// require_once './functions/function.php';
-//
-// foreach ($_SESSION['data'] as $key => $value) {
-//   if ($key!='birthday') {
-//     echo "$key: $value<br>";
-//   }else {
-//   echo "$key: ".validateData($value, 5)."<br>";
-// }
-// }
-     ?> -->
+  //print_r($_SESSION['data']);
+  echo "<h3>Dane pobrane z formularza</h3>"; // вывод текста "Dane pobrane z formularza" <h3></h3> - размер букв
+    foreach ($_SESSION['data'] as $key => $value) { /*начинаем  пошаговый перебор массива $_SESSION['data'] (который мы получили через сессию),
+      так же:
+      $key - это имя элемента формы (input name "test" будет в итоге test)
+      $value - значение что ввел пользователь (input name "test", value "value")*/
+      //echo "<br>".$key . " - " . $value . "<br>"; // первый способ вывода
+      echo "$key : $value, dlugość: " .strlen($value). "<br>"; /* второй способ вывода. Идет пошаговый вывод
+      каждого элемента массива $_SESSION['data']. также функция strlen($value) подсчитывает количество символов в переменной $value и
+      выводит результат на экран*/
+    }
+  echo "<hr><h3>Poprawione dane: </h3>"; // вывод текста "Dane pobrane z formularza" <h3></h3> - размер букв
+  require_once './functions/function.php'; /*Выражение require_once аналогично require за исключением того, что PHP проверит, включался ли
+  уже файл './functions/function.php', и если да, не будет включать его ещё раз. Вызвали файл с функциями*/
+    foreach ($_SESSION['data'] as $key => $value) { // запускаем пошаговый перебор всех елементов массива $_POST как $key => $value
+      if ($key!='birthday') { // задаем условие если значение переменной $key не ровняется значению 'birthday'
+        echo "$key: $value<br>"; // выводим значение переменной $key и переменной $value
+      }else { // если значение переменной $key ровняется значению 'birthday'
+        echo "$key: ".validateData($value, 4)."<br>"; // выводим значение переменной $key и значение функцию validateData с переменной $value
+      }
+    }
+     ?>
   </body>
 </html>
